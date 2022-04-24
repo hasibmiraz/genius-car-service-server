518169dfc523841827c95d30353c82b782cf197e
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-25 01:24:37 +0600

    Added JWT Authorization

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
+import jwt from 'jsonwebtoken';
 
 // call Express function
 const app = express();
@@ -12,6 +13,22 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden' });
+    }
+    console.log('decoded', decoded);
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@node-mongo-crud.j9vef.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -25,6 +42,15 @@ const run = async () => {
     await client.connect();
     const serviceCollection = client.db('geniusCar').collection('service');
     const orderCollection = client.db('geniusCar').collection('order');
+
+    // AUTH
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1D',
+      });
+      res.send(accessToken);
+    });
 
     // Get all services
     app.get('/services', async (req, res) => {
@@ -58,12 +84,15 @@ const run = async () => {
     });
 
     // Get users order
-    app.get('/order', async (req, res) => {
+    app.get('/order', verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const { email } = req.query;
-      const query = { email };
-      const cursor = orderCollection.find(query);
-      const orders = await cursor.toArray();
-      res.send(orders);
+      if (decodedEmail === email) {
+        const query = { email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      }
     });
     // Insert an order
     app.post('/order', async (req, res) => {
